d75045ea3fe54a1c8527887ebe4a6c3d1105b8b6
Svetlana Govyazina <govyazina@gmail.com>
2023-03-24 20:11:57 +0200

    task 4 added

--- tasks.js
+++ tasks.js
@@ -5,7 +5,7 @@
 // ("проКрАСТИНация") -> 6
 
 const getSumUpperCases = (str) => {
-     // return str.match(/[А-Я]/g).length
+    // return str.match(/[А-Я]/g).length
     let i = 0
     for (const strElement of str) {
         if (strElement === strElement.toUpperCase()) {
@@ -31,7 +31,23 @@ console.log('задача 2: ' + replace('aaa@bbb@ccc'))
 // Дана строка 'aaa bbb ccc'. Вырежите из нее слово 'bbb'
 
 const cut = (str, word) => {
-  return str.split(word).map(el => el.trim()).join(' ')
+    return str.split(word).map(el => el.trim()).join(' ')
 }
 
 console.log('задача 3: ' + cut('aaa bbb ccc', 'bbb'))
+
+// Задача 4
+// Дана переменная str, в которой хранится какой-либо текст.
+//     Реализуйте обрезание длинного текста по следующему принципу:
+//     если количество символов этого текста больше заданного
+// в переменной n, то в переменную result запишем первые n символов
+// строки str и добавим в конец троеточие '...'.
+//     В противном случае в переменную result запишем содержимое
+// переменной str.
+
+const threeDots = (str, n) => {
+    let result = str.slice(0, n) + '...'
+    return result
+}
+
+console.log('задача 3: ' + threeDots('Дана переменная str, в которой хранится какой-либо текст.', 25))
