377516fefe6d4fb4b4d6193a0e02a94221a4c4d4
Svetlana Govyazina <govyazina@gmail.com>
2023-03-24 19:43:34 +0200

    task 3 added

--- task2.js
+++ task2.js
@@ -5,4 +5,13 @@ const replace = (str) => {
     return str.replaceAll('@', '!')
 }
 
-console.log(replace('aaa@bbb@ccc'))
+console.log('задача 2: ' + replace('aaa@bbb@ccc'))
+
+// Задача 3
+// Дана строка 'aaa bbb ccc'. Вырежите из нее слово 'bbb'
+
+const cut = (str, word) => {
+  return str.split(word).map(el => el.trim()).join(' ')
+}
+
+console.log('задача 3: ' + cut('aaa bbb ccc', 'bbb'))
