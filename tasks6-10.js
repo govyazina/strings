a0deb35338e5bb11b0b89795319e32c300a73965
Svetlana Govyazina <govyazina@gmail.com>
2023-03-26 13:05:12 +0300

    task 7 added but has had problem with space

--- tasks6-10.js
+++ tasks6-10.js
@@ -9,3 +9,16 @@ const getSubstrings = (str) => {
 }
 
 console.log('задача 6: ' + getSubstrings('ahb acb aeb aeeb adcb axeb'))
+
+
+// Задача 7
+// Определите, что переданная строка является емэйлом.
+//     Примеры емэйлов для тестирования mymail@mail.ru, my.mail@mail.ru,
+// my-mail@mail.ru, my_mail@mail.ru, mail@mail.com, mail@mail.by,
+// mail@yandex.ru.
+
+const checkEmail = (email) => {
+    return email.match(/[-.\w]+@([\w-]+\.)+[\w-]+/g) ? email + ' ok' : email + ' not ok. check it!'
+}
+
+console.log('задача 7: ' + checkEmail('my_mail123@mail.ru'))
